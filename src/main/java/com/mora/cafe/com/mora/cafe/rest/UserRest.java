bcd87ef7ddca19163f51ed729372e50c3d32eee7
Manura Jay <gccodeguru@gmail.com>
2024-08-10 17:11:19 +0530

    MCF:DEV:Signup setupped.

---
+++ src/main/java/com/mora/cafe/com/mora/cafe/rest/UserRest.java
@@ -0,0 +1,16 @@
+package com.mora.cafe.com.mora.cafe.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@RequestMapping(path = "/user")
+public interface UserRest {
+
+    // create new user
+    @PostMapping(path = "/signup")
+    public ResponseEntity<String> signUp(@RequestBody(required = true)Map<String, String> requestMap);
+}
---
+++ src/main/java/com/mora/cafe/com/mora/cafe/service/UserService.java
@@ -0,0 +1,11 @@
+package com.mora.cafe.com.mora.cafe.service;
+
+import org.springframework.http.ResponseEntity;
+
+import java.util.Map;
+
+public interface UserService {
+
+    ResponseEntity<String> signUp(Map<String, String> requestMap);
+
+}
